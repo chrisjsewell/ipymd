64146b526907775611317f807c928ee1641e61fc
Chris Sewell <chrisj_sewell@hotmail.com>
2016-07-02 03:22:34 +0100

    fixed conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -104,7 +104,7 @@ import ipymd
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.dirname(inspect.getfile(pygauss)))
+sys.path.insert(0, os.path.dirname(inspect.getfile(ipymd)))
 
 # -- General configuration ------------------------------------------------
 
