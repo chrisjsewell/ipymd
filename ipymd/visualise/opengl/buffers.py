96281edaf092799356e8aaa0347f3a2aba4d912a
Chris Sewell <chrisj_sewell@hotmail.com>
2016-07-16 06:45:16 +0100

    replaced * import

--- ipymd/visualise/opengl/buffers.py
+++ ipymd/visualise/opengl/buffers.py
@@ -1,4 +1,4 @@
-from OpenGL.GL import *
+import OpenGL.GL as ogl
 from OpenGL.raw import GL
 from OpenGL.arrays import ArrayDatatype as ADT
 
@@ -7,8 +7,8 @@ import numpy as np
 class VertexBuffer(object):
 
   def __init__(self, data, usage):
-    self.buffer = GLuint(0)
-    self.buffer = glGenBuffers(1)
+    self.buffer = ogl.GLuint(0)
+    self.buffer = ogl.glGenBuffers(1)
     self.usage = usage
     self.data = data
     
@@ -16,53 +16,53 @@ class VertexBuffer(object):
     if data.dtype == np.float:
       Warning('This float array is 64 bit')
     
-    glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
-    glBufferData(GL_ARRAY_BUFFER, ADT.arrayByteCount(data),
+    ogl.glBindBuffer(ogl.GL_ARRAY_BUFFER, self.buffer)
+    ogl.glBufferData(ogl.GL_ARRAY_BUFFER, ADT.arrayByteCount(data),
                  ADT.voidDataPointer(data), usage)
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    ogl.glBindBuffer(ogl.GL_ARRAY_BUFFER, 0)
 
   def __del__(self):
     # this causes an error otherwise
-    if bool(glDeleteBuffers):
-      glDeleteBuffers(1, GLuint(self.buffer))
+    if bool(ogl.glDeleteBuffers):
+      ogl.glDeleteBuffers(1, ogl.GLuint(self.buffer))
     else:
       return
 
   def bind(self):
-    glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
+    ogl.glBindBuffer(ogl.GL_ARRAY_BUFFER, self.buffer)
     
   def unbind(self):
-    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    ogl.glBindBuffer(ogl.GL_ARRAY_BUFFER, 0)
 
   def set_data(self, data):
     self.bind()
-    glBufferData(GL_ARRAY_BUFFER, ADT.arrayByteCount(data), ADT.voidDataPointer(data), self.usage)
+    ogl.glBufferData(ogl.GL_ARRAY_BUFFER, ADT.arrayByteCount(data), ADT.voidDataPointer(data), self.usage)
     self.unbind()
       
   def bind_colors(self, size, type, stride=0):
     self.bind()
-    glColorPointer(size, type, stride, None)
+    ogl.glColorPointer(size, type, stride, None)
 
   def bind_edgeflags(self, stride=0):
     self.bind()
-    glEdgeFlagPointer(stride, None)
+    ogl.glEdgeFlagPointer(stride, None)
 
   def bind_indexes(self, type, stride=0):
     self.bind()
-    glIndexPointer(type, stride, None)
+    ogl.glIndexPointer(type, stride, None)
 
   def bind_normals(self, type, stride=0):
     self.bind()
-    glNormalPointer(type, stride, None)
+    ogl.glNormalPointer(type, stride, None)
 
   def bind_texcoords(self, size, type, stride=0):
     self.bind()
-    glTexCoordPointer(size, type, stride, None)
+    ogl.glTexCoordPointer(size, type, stride, None)
 
   def bind_vertexes(self, size, type, stride=0):
     self.bind()
-    glVertexPointer(size, type, stride, None)
+    ogl.glVertexPointer(size, type, stride, None)
     
-  def bind_attrib(self, attribute, size, type, normalized=GL_FALSE, stride=0):
+  def bind_attrib(self, attribute, size, type, normalized=ogl.GL_FALSE, stride=0):
     self.bind()
-    glVertexAttribPointer(attribute, size, type, normalized, stride, None)
+    ogl.glVertexAttribPointer(attribute, size, type, normalized, stride, None)
